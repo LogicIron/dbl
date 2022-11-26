a8f612c9ef40a3b8c63587a3633ab8e484b16a82
LogicIron <118935919+LogicIron@users.noreply.github.com>
2022-11-26 13:58:20 +0100

    Add files via upload

---
+++ Dbl.lua
@@ -0,0 +1,317 @@
+gg.alert("Nouvelle Protection avec code")
+gg.alert("Le code changera souvent ")
+Username = "Logic"
+Password = "Apo"
+Result = gg.prompt({"👤 Usᴇʀɴᴀᴍᴇ","🔒 Pᴀssᴡᴏʀᴅ"},nil,{"text",
+"text"})
+if Result[1] == Username and Result[2] == Password then
+gg.toast('ON ✅')
+else
+gg.toast("ce n est pas le bon code")
+os.exit()
+end
+
+
+
+gg.alert("Fuck Arrow le voleur")
+gg.alert("by _LogicIron X Corbloss")
+gg.alert("Concernant le PVP 14 stars certain ne pourront pas pvp, si c'est le cas mettez le dans le salon aide")
+gg.toast('Sharingan')
+gg.toast('Sharingan')
+gg.toast('Sharingan')
+gg.toast('Sharingan')
+gg.alert(" Hack Kai!!!!")
+
+gg.setVisible(true)
+LuaLibraryTool = -1
+function HOME()
+
+MENU = gg.multiChoice({
+        "Ultimate Injection  ", --multiChoice
+        "Manuel Injection  ", --multiChoice
+        "Boost Equipements  ", --multiChoice
+        "BY PASS PVP 14STARS  ", --multiChoice
+        "Force drop classic  ", --multiChoice
+        "+15% LF Force drop  ", --multiChoice
+        "Single Force drop du desespoir  ", --multiChoice
+        "Exit" --EndScript
+}, nil, "by _LogicIron X Corbloss")
+if MENU == nil then
+  else
+   if MENU[1] == true then --multiChoice
+      MENU1()
+     end
+if MENU[2] == true then --multiChoice
+      MENU2()
+     end
+if MENU[3] == true then --multiChoice
+      MENU3()
+     end
+if MENU[4] == true then --multiChoice
+      MENU4()
+     end
+if MENU[5] == true then --multiChoice
+      MENU5()
+     end
+if MENU[6] == true then --multiChoice
+      MENU6()
+     end
+if MENU[7] == true then --multiChoice
+      MENU7()
+     end
+if MENU[8] == true then --EndScript
+      LOBBY()
+     end        
+    end
+  LuaLibraryTool = -1
+end
+
+function MENU1() --multiChoice
+
+gg.searchNumber('250;500::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('500;750::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('750;1000::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('1250;1500::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('1750;2000::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('2250;2500::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('2750;3000::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('3250;3500::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('600;1200::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('2400;1800::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('3000;2400::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('3600;3000::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('4200;3600::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('4800;4200::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('5400;6000::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('6600;7200::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+gg.searchNumber('7800;8400::512', gg.TYPE_DWORD  )
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.editAll(9999, gg.TYPE_DWORD , FREEZE_NORMAL)
+gg.freeze = true
+gg.clearResults(5000)
+
+
+
+
+
+
+
+
+gg.toast('14 Stars injector')
+gg.alert("Va checker ta box")
+gg.alert("Si cela ne fonctionne pas relance ou essaye en manuel , tu peux très bien aussi demander de l aide")
+
+
+
+end
+
+function MENU2() --multiChoice
+
+menu =gg.prompt({
+  "écris tes âme suivi de ;9999"
+}, {"0"}, {"number"})
+gg.searchNumber(menu[1], gg.TYPE_DWORD)
+t1 = gg.getResults(1300)
+gg.getResults(1300)
+gg.alert('attend 5 sec')
+gg.sleep(5000)
+gg.editAll("9999", gg.TYPE_DWORD)
+gg.clearResults(5000)
+gg.alert('check ta box')
+
+
+
+end
+
+function MENU3() --multiChoice
+
+
+
+gg.alert("Permet d'avoir de meilleur equipements en les montants ")
+
+gg.alert("Fonction encor en Béta ")
+
+gg.searchNumber(58, gg.TYPE_WORD)
+gg.getResults(2)
+gg.sleep(100)
+gg.editAll(28, gg.TYPE_WORD)
+gg.sleep(1000)
+
+gg.alert("Boost activer va monter tes equipemenents mtn  ")
+
+
+end
+
+function MENU4() --multiChoice
+
+gg.alert("By pass 14 star pvp ") 
+
+gg.alert(", wait the update, because the new update game - pvp bypass to 27/08/22 Soon")
+
+end
+
+function MENU5() --multiChoice
+
+gg.alert("Laisser le Force Drop s'activer avant d'invoquer")
+
+gg.searchNumber(0.0000001, gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000001, gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+gg.searchNumber(0.0000002,gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000002,gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+gg.searchNumber(0.0000003,gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000003,gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+gg.searchNumber(0.0000004,gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000004,gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+gg.searchNumber(0.0000005,gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000005,gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+gg.searchNumber(0.0000006,gg.TYPE_FLOAT)
+gg.getResults(100000)
+gg.sleep(1000)
+gg.editAll(50, gg.TYPE_FLOAT)
+gg.sleep(1000)
+gg.editAll(0.0000006,gg.TYPE_FLOAT)
+gg.clearResults()
+
+
+
+end
+
+
+
+function LOBBY() --End Script
+print("by _LogicIron X Corbloss")
+gg.skipRestoreState()
+  gg.setVisible(true)
+  os.exit()
+end
+while true do
+  if gg.isVisible(true) then
+    LuaLibraryTool = 1
+    gg.setVisible(false)
+  end
+  if LuaLibraryTool == 1 then
+    HOME()
+  end
+end
+    
+
+
+    
+
+
